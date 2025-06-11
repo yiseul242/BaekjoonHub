1eefe96d410fb3cb4f3d0128477fe096e38af1ce
yiseul242 <yiseul242@gmail.com>
2025-06-11 17:39:06 +0900

    [Bronze V] Title: 꼬마 정민, Time: 112 ms, Memory: 15828 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String s = br.readLine();
+        StringTokenizer st = new StringTokenizer(s);
+        
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+        
+        bw.write((a + b + c) + "\n");
+        bw.close();
+    }
+}
